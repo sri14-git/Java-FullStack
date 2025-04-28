f719d68bb2f110cb54c86c5c310eafb5aea6ccda
sri14-git <srivarshan14@gmail.com>
2025-04-28 08:21:34 +0530

    First commit

--- Multi-Threading/src/main/java/org/example/Main.java
+++ Multi-Threading/src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 package org.example;
+class Counter extends Thread{
 
+    @Override
+    public void run() {
+        try {
+            counting();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void counting() throws InterruptedException {
+        for (int i=0;i<=10;i++){
+            sleep(300);
+            System.out.println("Counting "+i);
+        }
+    }
+}
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Counter count= new Counter();
+        count.start();
+        Counter count1=new Counter();
+        count1.start();
+
 
 
     }
